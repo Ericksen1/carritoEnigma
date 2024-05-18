4d0b9d8df13282b43f7ae7cdb76018fe09bfff80
RealCodeStream <github2023JP@gmail.com>
2024-05-17 19:01:09 -0500

    Carrito Terminado

--- Enigma/src/app/carrito/carrito.page.ts
+++ Enigma/src/app/carrito/carrito.page.ts
@@ -1,21 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonButton, IonLabel,IonThumbnail
+  , IonList, IonItem, IonBackButton, IonIcon,
+ } from '@ionic/angular/standalone';
+import { DatabaseService} from './database.service'; // Importa servicio de datos
+import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
+
+interface Item {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  total: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-carrito',
   templateUrl: './carrito.page.html',
   styleUrls: ['./carrito.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+     RouterLink, IonThumbnail,IonList, IonItem, IonIcon, IonBackButton, IonList, IonLabel, IonButton, ]
 })
 export class CarritoPage implements OnInit {
-
-  constructor() { }
+  items: Item[] = [];
+  constructor(private databaseService: DatabaseService) { }
 
   ngOnInit() {
-    console.log
+    this.loadData();
+  }
+  loadData() {
+    this.databaseService.getData().subscribe((data: any[]) => {
+      this.items = data; 
+    });
   }
 
+  increaseQuantity(item: Item) {
+    item.quantity++;
+    item.total = item.quantity * item.price;
+  }
+
+  decreaseQuantity(item: Item) {
+    if (item.quantity > 1) {
+      item.quantity--;
+      item.total = item.quantity * item.price;
+    }
+  }
+
+  removeItem(item: Item) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  getTotal(): number {
+    //Aqui se puede usar una de las dos 
+    return this.items.reduce((total, item) => total + item.total, 0);
+    /* let total = 0;
+  for (let item of this.items) {
+    total += item.price;
+  }
+  return total;*/ 
+  }
 }
--- Enigma/src/app/carrito/database.service.ts
+++ Enigma/src/app/carrito/database.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,15 @@ import { Injectable } from '@angular/core';
 export class DatabaseService {
 
   constructor() { }
+
+  getData(): Observable<any[]> {
+    
+    return of([
+      { id: 1, name: 'Producto 1', price: 10, quantity: 5, total: 50, image: '#' },
+      { id: 2, name: 'Producto 2', price: 15, quantity: 5, total: 75, image: '#' },
+      { id: 3, name: 'Producto 3', price: 15, quantity: 5, total: 75, image: '#' },
+      { id: 3, name: 'Producto 4', price: 15, quantity: 5, total: 75, image: '#' },
+      
+    ]);
+  }
 }
