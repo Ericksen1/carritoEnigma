32e9a20b4245106d6e4919c1befa83d21d8c5182
RealCodeStream <github2023JP@gmail.com>
2024-05-17 19:25:08 -0500

    otra corrección

--- Enigma/src/app/carrito/database.service.ts
+++ Enigma/src/app/carrito/database.service.ts
@@ -13,8 +13,8 @@ export class DatabaseService {
     return of([
       { id: 1, name: 'Producto 1', price: 10, quantity: 5, total: 50, image: '#' },
       { id: 2, name: 'Producto 2', price: 15, quantity: 5, total: 75, image: '#' },
-      { id: 3, name: 'Producto 3', price: 15, quantity: 5, total: 75, image: '#' },
-      { id: 3, name: 'Producto 4', price: 15, quantity: 5, total: 75, image: '#' },
+      { id: 3, name: 'Producto 3', price: 20, quantity: 5, total: 100, image: '#' },
+      { id: 3, name: 'Producto 4', price: 25, quantity: 5, total: 125, image: '#' },
       
     ]);
   }
